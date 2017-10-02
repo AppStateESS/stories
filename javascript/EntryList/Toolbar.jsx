58b711fd0671f32dc802fc8b58248aa0767a5164
Matthew McNaney <mcnaney@gmail.com>
2017-10-02 08:36:57 -0400

    Look and function of toolbar changed

--- javascript/EntryList/Toolbar.jsx
+++ javascript/EntryList/Toolbar.jsx
@@ -2,36 +2,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Toolbar = ({entry, publish, deleteStory,}) => {
+const Toolbar = ({entry, publish, deleteStory}) => {
   let buttons
-  let title = <em>Select a story to edit</em>
-  if (entry !== null) {
-    title = <strong>{entry.title}</strong>
 
-    buttons = <span>
-      <a
-        href={`stories/Entry/${entry.id}/edit`}
-        className="btn btn-sm btn-primary ml-1">
-        <i className="fa fa-pencil"></i>&nbsp; Edit
-      </a>
-      <a href={`stories/Entry/${entry.id}`} className="btn btn-sm btn-default ml-1">
-        <i className="fa fa-eye"></i>&nbsp; View
-      </a>
-      <button className="btn btn-sm btn-danger ml-1" onClick={deleteStory}>
-        <i className="fa fa-trash-o"></i>&nbsp;Delete</button>
-    </span>
-  }
+  buttons = <span>
+    <a
+      href={`stories/Entry/${entry.id}/edit`}
+      className="btn btn-sm btn-primary ml-1">
+      <i className="fa fa-pencil"></i>&nbsp; Edit
+    </a>
+    <a href={`stories/Entry/${entry.id}`} className="btn btn-sm btn-default ml-1">
+      <i className="fa fa-eye"></i>&nbsp; View
+    </a>
+    {entry.published ? 'Published' : 'Publish now'}
+    <button className="btn btn-sm btn-danger ml-1" onClick={deleteStory}>
+      <i className="fa fa-trash-o"></i>&nbsp;Delete</button>
+  </span>
   return (
-    <div className="row entry-toolbar">
-      <div className="col-sm-2">
-        <a className="btn btn-sm btn-success" href="./stories/Entry/create">
-          <i className="fa fa-book"></i>&nbsp;
-          Create</a>
-      </div>
-      <div className="col-sm-5">
-        {title}
-      </div>
-      <div className="col-sm-5">
+    <div className="row">
+      <div className="col-sm-12">
         {buttons}
       </div>
     </div>
@@ -41,7 +30,7 @@ const Toolbar = ({entry, publish, deleteStory,}) => {
 Toolbar.propTypes = {
   entry: PropTypes.object,
   publish: PropTypes.func.isRequired,
-  deleteStory: PropTypes.func.isRequired
+  deleteStory: PropTypes.func.isRequired,
 }
 
 export default Toolbar
