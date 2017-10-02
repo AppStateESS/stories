09300b0fe8bab39bea9cede5cc814505f84bb4b4
Matthew McNaney <mcnaney@gmail.com>
2017-10-02 08:44:35 -0400

    Controls for the list view

---
+++ javascript/EntryList/Listcontrols.jsx
@@ -0,0 +1,77 @@
+'use strict'
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ListControls = (props) => {
+  const {search, handleChange, updateSort, sortBy} = props
+  return (
+    <div className="mb-1 row">
+      <div className="col-sm-3">
+        <a className="btn btn-sm btn-success" href="./stories/Entry/create">
+          <i className="fa fa-book"></i>&nbsp; Create a new story</a>
+      </div>
+      <div className="col-sm-3">
+        <SortBy {...props}/>
+      </div>
+      <div className="col-sm-6">
+        <input
+          className="form-control"
+          type="text"
+          value={search}
+          onChange={handleChange}
+          placeholder="Search..."/>
+      </div>
+    </div>
+  )
+}
+
+ListControls.propTypes = {
+  search: PropTypes.string,
+  handleChange: PropTypes.func,
+  updateSort: PropTypes.func,
+  sortBy: PropTypes.string,
+}
+
+const SortBy = ({sortBy, updateSort}) => {
+  let sortStr
+  switch (sortBy) {
+
+    case 'updateDate':
+      sortStr = 'last updated'
+      break
+
+    case 'title':
+      sortStr = 'title'
+      break
+
+    default:
+      sortStr = 'date published'
+      break
+  }
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-default btn-sm dropdown-toggle"
+        type="button"
+        id="dropdownMenu1"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="true">
+        Sort by {sortStr}&nbsp;
+        <span className="caret"></span>
+      </button>
+      <ul className="dropdown-menu sortby" aria-labelledby="dropdownMenu1">
+        <li className="pointer" onClick={updateSort.bind(null, 'publishDate')}>Published</li>
+        <li className="pointer" onClick={updateSort.bind(null, 'updateDate')}>Updated</li>
+        <li className="pointer" onClick={updateSort.bind(null, 'title')}>Title</li>
+      </ul>
+    </div>
+  )
+}
+
+SortBy.propTypes = {
+  sortBy: PropTypes.string,
+  updateSort: PropTypes.func,
+}
+
+export default ListControls
