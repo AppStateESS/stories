9283a010f58ac76936855a71c059bad7774cc883
Matthew McNaney <mcnaney@gmail.com>
2017-10-12 15:06:57 -0400

    Publish sent entry object

--- javascript/PublishBar/index.jsx
+++ javascript/PublishBar/index.jsx
@@ -2,6 +2,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Publish from './Publish.jsx'
-/* global entryId, published, publishDate, title, tags */
-const props = {entryId, published, publishDate, title}
-ReactDOM.render(<Publish {...props}/>, document.getElementById('PublishBar'))
+/* global entry */
+ReactDOM.render(<Publish {...entry}/>, document.getElementById('PublishBar'))
