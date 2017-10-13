6e547367ce9a4b46a228dea28d816f1ac9d712c4
Matthew McNaney <mcnaney@gmail.com>
2017-10-13 12:07:12 -0400

    Added list story format buttons

--- javascript/Settings/Settings.jsx
+++ javascript/Settings/Settings.jsx
@@ -14,28 +14,34 @@ export default class Settings extends Component {
       listStoryAmount: 3,
       showFeatures: 0,
       featureNumber: 6,
+      listStoryFormat: 0,
     }
     this.setListStories = this.setListStories.bind(this)
     this.setListStoryAmount = this.setListStoryAmount.bind(this)
     this.setShowFeatures = this.setShowFeatures.bind(this)
     this.setShowFeatureNumber = this.setShowFeatureNumber.bind(this)
+    this.setListStoryFormat = this.setListStoryFormat.bind(this)
 
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState(this.props.settings)
   }
 
   saveSetting(param, value) {
     $.post('./stories/Settings', {
       param: param,
-      value: value,
+      value: value
     }, null, 'json')
   }
 
   setListStories(value) {
     this.saveSetting('listStories', value)
-    this.setState({listStories: value ? 1 : 0})
+    this.setState({
+      listStories: value
+        ? 1
+        : 0
+    })
   }
 
   setListStoryAmount(value) {
@@ -45,7 +51,11 @@ export default class Settings extends Component {
 
   setShowFeatures(value) {
     this.saveSetting('showFeatures', value)
-    this.setState({showFeatures: value ? 1 : 0})
+    this.setState({
+      showFeatures: value
+        ? 1
+        : 0
+    })
   }
 
   setShowFeatureNumber(value) {
@@ -53,20 +63,35 @@ export default class Settings extends Component {
     this.setState({featureNumber: value})
   }
 
+  setListStoryFormat(value) {
+    this.saveSetting('listStoryFormat', value)
+    this.setState({listStoryFormat: value})
+  }
+
   render() {
     const amountButtons = [
       {
         value: 3,
-        label: 3,
+        label: 3
       }, {
         value: 6,
-        label: 6,
+        label: 6
       }, {
         value: 9,
-        label: 9,
+        label: 9
       }, {
         value: 12,
-        label: 12,
+        label: 12
+      },
+    ]
+
+    const formatButton = [
+      {
+        value: 0,
+        label: 'Summary',
+      }, {
+        value: 1,
+        label: 'Full',
       },
     ]
 
@@ -80,7 +105,7 @@ export default class Settings extends Component {
               checked={this.state.listStories}
               label="List stories on front page"/>
           </div>
-          <div className="indent">
+          <div className="indent clearfix">
             <div className="pull-left mr-1">
               <ButtonGroup
                 buttons={amountButtons}
@@ -88,6 +113,15 @@ export default class Settings extends Component {
                 match={this.state.listStoryAmount}/>
             </div>
             <div>Number of stories per page</div>
+          </div>
+          <div className="indent clearfix mt-1">
+            <div className="pull-left mr-1">
+              <ButtonGroup
+                buttons={formatButton}
+                handle={this.setListStoryFormat}
+                match={this.state.listStoryFormat}/>
+            </div>
+            <div>Story display type:</div>
           </div>
         </div>
         <div className="settings">
@@ -112,4 +146,6 @@ export default class Settings extends Component {
   }
 }
 
-Settings.propTypes = {}
+Settings.propTypes = {
+  settings: PropTypes.object
+}
