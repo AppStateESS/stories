a99f5143ab17aa6de593ce85f916b003c5f22705
Matthew McNaney <mcnaney@gmail.com>
2017-10-17 12:32:15 -0400

    twitterwidget added as acceptable tag [more]
    Added settings for thumbnail creation

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -9,13 +9,23 @@
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  * @license https://opensource.org/licenses/MIT
  */
-define('STORIES_FRIENDLY_ERROR', true);
+define('STORIES_FRIENDLY_ERROR', false);
 
-define('STORIES_REACT_DEV', false);
+define('STORIES_REACT_DEV', true);
 
 define('STORIES_CONTENT_TAGS',
-        'b,strong,em,i,p,img,iframe,h2,h3,h4,h5,a,style,blockquote,ul,ol,li,figure,figcaption,div');
+        'b,strong,em,i,p,img,iframe,h2,h3,h4,h5,a,style,blockquote,ul,ol,li,figure,figcaption,div,twitterwidget');
 
 define('STORIES_SUMMARY_TAGS', 'b,strong,em,i,a,style,ul,ol,li,p');
 
 define('STORIES_DAY_THRESHOLD', 3);
+
+// pics with the width:height ratio below will cropped to landscape
+define('STORIES_ORIENTATION_RATIO', 1.3);
+
+// width/height on thumb ratios
+define('STORIES_LANDSCAPE_THUMB_WIDTH', 340);
+define('STORIES_LANDSCAPE_THUMB_HEIGHT', 100);
+
+define('STORIES_PORTRAIT_THUMB_WIDTH', 180);
+define('STORIES_PORTRAIT_THUMB_HEIGHT', 260);
