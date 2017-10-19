92ff0ade07a13db6ccb03dc00c6fa560807f7d95
Matthew McNaney <mcnaney@gmail.com>
2017-10-19 13:21:44 -0400

    api files added

---
+++ api/Facebook/script.js
@@ -0,0 +1,11 @@
+(function (d, s, id) {
+  let js
+  const fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) {
+    return
+  }
+  js = d.createElement(s)
+  js.id = id
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10"
+  fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
