37f84737bd79f9213f0204f8c24d9b59c7fd543c
Matthew McNaney <mcnaney@gmail.com>
2017-10-20 10:11:18 -0400

    Added author

--- class/Factory/SettingsFactory.php
+++ class/Factory/SettingsFactory.php
@@ -23,6 +23,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+/**
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ */
 
 namespace stories\Factory;
 
