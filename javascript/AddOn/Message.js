1b167bf332228e09bc6f609f18f4db69c648867a
Matthew McNaney <mcnaney@gmail.com>
2017-10-27 09:21:37 -0400

    Added message script. Changed from class

---
+++ javascript/AddOn/Message.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Message = (props) => {
+  let icon = ''
+  switch (props.type) {
+    case 'danger':
+      icon = 'fa fa-exclamation-triangle'
+      break
+
+    case 'success':
+      icon = 'fa fa-thumbs-o-up'
+      break
+
+    case 'info':
+      icon = 'fa fa-info-circle'
+      break
+
+    case 'warning':
+      icon = 'fa fa-hand-paper-o'
+      break
+  }
+
+  let messageType = 'alert alert-dismissible alert-' + props.type
+  return (
+    <div className={messageType} role="alert">
+      <button
+        type="button"
+        onClick={props.onClose}
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <i className={icon}></i>&nbsp; {props.children}
+    </div>
+  )
+}
+
+Message.propTypes = {
+  type: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element,]),
+  onClose: PropTypes.func,
+}
+
+Message.defaultProps = {
+  type: 'info'
+}
+
+export default Message
