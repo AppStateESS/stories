5cf18723d87711da62dc36aa046de66b29ccbca2
Matthew McNaney <mcnaney@gmail.com>
2017-10-27 12:39:02 -0400

    Story selection and clear working

--- javascript/Feature/DisplayColumn.js
+++ javascript/Feature/DisplayColumn.js
@@ -1,20 +1,46 @@
 'use strict'
 import React from 'react'
 import PropTypes from 'prop-types'
+import Select from 'react-select'
+import 'react-select/dist/react-select.min.css'
 
-const DisplayColumn = ({format, bsClass, entry,}) => {
+const DisplayColumn = ({
+  format,
+  bsClass,
+  entry,
+  stories,
+  applyStory,
+  clearStory,
+}) => {
   const _class = 'story-feature ' + format
   const thumbnailStyle = {
     backgroundImage: `url('${entry.thumbnail}')`,
-    backgroundPosition: '50% 50%'
+    backgroundPosition: '50% 50%',
   }
+
+  let storyList = <em>No published stories available</em>
+  if (stories !== undefined) {
+    let storyOptions = stories.map(function (value) {
+      return {value: value.id, label: value.title,}
+    })
+    storyList = (
+      <div>
+        <Select
+          options={storyOptions}
+          value={0}
+          onChange={applyStory}/>
+          <button className="btn btn-primary" onClick={clearStory}>Clear</button>
+      </div>
+    )
+  }
+
   return (
     <div className={bsClass}>
       <div className={_class}>
         <div className="story-thumbnail" style={thumbnailStyle}></div>
         <div className="story-content">
           <div className="story-title">
-            <a>
+            <a title="Link to story">
               <h3>{entry.title}</h3>
             </a>
           </div>
@@ -23,6 +49,7 @@ const DisplayColumn = ({format, bsClass, entry,}) => {
           </div>
         </div>
       </div>
+      {storyList}
     </div>
   )
 }
@@ -30,7 +57,9 @@ const DisplayColumn = ({format, bsClass, entry,}) => {
 DisplayColumn.propTypes = {
   bsClass: PropTypes.string,
   format: PropTypes.string,
-  entry: PropTypes.object
+  entry: PropTypes.object,
+  stories: PropTypes.array,
+  applyStory: PropTypes.func
 }
 
 DisplayColumn.defaultTypes = {}
