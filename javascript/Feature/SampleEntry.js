dc89ad0c4bb64906c777a86d01c89342fc3743c6
Matthew McNaney <mcnaney@gmail.com>
2017-11-09 13:45:06 -0500

    changed to work with new feature entry formats

--- javascript/Feature/SampleEntry.js
+++ javascript/Feature/SampleEntry.js
@@ -1,5 +1,5 @@
 const SampleStory = {
-  id: 0,
+  entryId: 0,
   title: 'Example title',
   strippedSummary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean risus diam, iacu' +
       'lis vel arcu sit amet, molestie faucibus diam.',
@@ -8,7 +8,7 @@ const SampleStory = {
 }
 
 const SampleEntry = () => {
-  return {id: 0, x: 50, y: 50, story: SampleStory}
+  return {entryId: 0, x: 50, y: 50, story: SampleStory}
 }
 
 export default SampleEntry
