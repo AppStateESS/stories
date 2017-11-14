8547db32a30a35aee1a49076b5ce4cb3862a1a17
Matthew McNaney <mcnaney@gmail.com>
2017-11-14 08:08:38 -0500

    Matched row order. Fixed production compile bug.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,54 +12,27 @@ module.exports = {
     chunkFilename: '[name].[chunkhash:8].chunk.js',
   },
   externals: {
-    jquery: 'jQuery',
     $: 'jQuery',
+    EntryForm: 'EntryForm',
   },
   resolve: {
-    extensions: ['.js', '.jsx',],
+    extensions: [
+      '.js', '.jsx',
+    ],
     alias: {
-      'jquery-ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js',
+      'jquery-ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js'
     },
   },
-  module: {
-    rules: [
-      {
-        test: require.resolve('blueimp-file-upload'),
-        loader: 'imports-loader?define=>false'
-      },
-      {
-        test: require.resolve('medium-editor-insert-plugin'),
-        loader: 'imports-loader?define=>false'
-      },
-      {
-        test: /\.jsx?/,
-        include: setup.APP_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react',]
-        }
-      }, {
-        test: [
-          /\.js$/, /\.es6$/, /\.jsx$/,
-        ],
-        exclude: /node_modules/,
-        loader: WebpackStripLoader.loader('console.log')
-      }, {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }, {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-    ]
-  },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.[chunkhash:8].js'}),
+    new webpack.optimize.CommonsChunkPlugin(
+      {name: 'vendor', filename: 'vendor.[chunkhash:8].js'}
+    ),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
+
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
@@ -73,6 +46,37 @@ module.exports = {
         screw_ie8: true
       }
     }),
+
     new AssetsPlugin({filename: 'assets.json', prettyPrint: true}),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: require.resolve('blueimp-file-upload'),
+        loader: 'imports-loader?define=>false'
+      }, {
+        test: require.resolve('medium-editor-insert-plugin'),
+        loader: 'imports-loader?define=>false'
+      }, {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }, {
+        test: /\.jsx?/,
+        include: setup.APP_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react',]
+        }
+      }, {
+        test: [
+          /\.js$/, /\.es6$/, /\.jsx$/,
+        ],
+        exclude: /node_modules/,
+        loader: WebpackStripLoader.loader('console.log')
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+    ]
+  }
 }
