2f46bbb26ae21cecd913f0a8b5509fe56c5b2393
Matthew McNaney <mcnaney@gmail.com>
2017-12-01 12:10:41 -0500

    Added AuthorList and Navbar

--- exports.js
+++ exports.js
@@ -4,10 +4,12 @@ exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 exports.entry = {
   MediumEditorPack: exports.APP_DIR + '/EntryForm/MediumEditorPack.js',
   EntryList: exports.APP_DIR + '/EntryList/index.jsx',
+  AuthorList: exports.APP_DIR + '/AuthorList/index.jsx',
   EntryForm: exports.APP_DIR + '/EntryForm/EntryForm.js',
   PublishBar: exports.APP_DIR + '/PublishBar/index.jsx',
   TagBar: exports.APP_DIR + '/TagBar/index.jsx',
   Settings: exports.APP_DIR + '/Settings/index.jsx',
   Feature: exports.APP_DIR + '/Feature/index.jsx',
+  Navbar: exports.APP_DIR + '/Navbar/index.jsx',
   vendor: ['react', 'react-dom']
 }
