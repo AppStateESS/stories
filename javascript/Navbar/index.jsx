d3162912ca0fb09420393eae919455b40a92b4c2
Matthew McNaney <mcnaney@gmail.com>
2017-12-04 15:58:53 -0500

    Added button to view story from edit.

--- javascript/Navbar/index.jsx
+++ javascript/Navbar/index.jsx
@@ -3,6 +3,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Navbar from '../AddOn/Navbar'
 
-ReactDOM.render(<Navbar header="Edit story" rightSide={<li><a><em>Story status:</em> <span id="story-status">{status}</span></a></li>}/>, document.getElementById(
-  'Navbar'
-))
+/* global entry */
+
+const rightSide = (
+  <li>
+    <a>
+      <em>
+        Story status:
+      </em>&nbsp;
+      <span id="story-status">
+        {status}
+      </span>
+    </a>
+  </li>
+)
+
+const leftSide = (
+  <li>
+    <a href={`./stories/${entry.urlTitle}`}>View story</a>
+  </li>
+)
+
+
+ReactDOM.render(
+  <Navbar header="Edit story" rightSide={rightSide} leftSide={leftSide}/>,
+  document.getElementById(
+    'Navbar'
+  )
+)
