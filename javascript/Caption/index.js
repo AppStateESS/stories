a1e963f0c9417e8f9305d7e1a54e2ab33e053d6f
Matthew McNaney <mcnaney@gmail.com>
2017-12-14 12:16:19 -0500

    Added script to fix captions not lining up with centered images

---
+++ javascript/Caption/index.js
@@ -0,0 +1,18 @@
+require("expose-loader?$!jquery")
+
+/* global $ */
+
+const resizeCaption = () => {
+  const containerWidth = $('.container').width()
+  $('figure').each(function (i, obj) {
+    const image = $(obj).find('img')
+    const caption = $(obj).find('figcaption')
+    const imageWidth = image.width()
+    const leftMargin = (containerWidth - imageWidth) / 2
+    caption.css('margin-left', leftMargin)
+  })
+
+}
+
+resizeCaption()
+$(window).resize(resizeCaption)
