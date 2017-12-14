f9e4c7770956592ec4e4ddb26d623a3fb3a90a57
Matthew McNaney <mcnaney@gmail.com>
2017-12-14 11:56:31 -0500

    Tags are forced to lowercase.

--- javascript/AddOn/Tags.jsx
+++ javascript/AddOn/Tags.jsx
@@ -7,11 +7,17 @@ export default class Tags extends React.Component {
   constructor(props) {
     super(props)
     this.newOptionClick = this.newOptionClick.bind(this)
+    this.forceInput = this.forceInput.bind(this)
   }
 
   newOptionClick(value) {
     this.props.newOptionClick(value)
     this.refs.tagSelect.select.setState({inputValue: ''})
+  }
+
+  forceInput(val) {
+    const toLower = val.toLowerCase()
+    return toLower
   }
 
   render() {
@@ -23,6 +29,8 @@ export default class Tags extends React.Component {
           options={this.props.tags}
           value={this.props.entryTags}
           onNewOptionClick={this.newOptionClick}
+          onInputChange={this.forceInput}
+          ignoreCase={true}
           onChange={this.props.tagChange}/>
       </div>
     )
