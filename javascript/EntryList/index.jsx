31f913850b382ba722d815837e56fca83a6e5c89
Matthew McNaney <mcnaney@gmail.com>
2018-01-23 09:51:57 -0500

    Removed segmentSize

--- javascript/EntryList/index.jsx
+++ javascript/EntryList/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom'
 import EntryList from './EntryList.jsx'
 
 /* global segmentSize */
-ReactDOM.render(<EntryList segmentSize={segmentSize}/>, document.getElementById('EntryList'))
+ReactDOM.render(<EntryList/>, document.getElementById('EntryList'))
