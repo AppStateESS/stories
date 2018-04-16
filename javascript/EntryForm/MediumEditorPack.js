3e3859509c21b8ba432760a9e42e9874b78a3e6f
Matthew McNaney <mcnaney@gmail.com>
2018-04-16 13:16:39 -0400

    Forced H2 - H4 icon updates to font awesome 5

--- javascript/EntryForm/MediumEditorPack.js
+++ javascript/EntryForm/MediumEditorPack.js
@@ -1,3 +1,4 @@
+/* global $, MediumEditor */
 require("expose-loader?$!jquery")
 import 'blueimp-file-upload/js/vendor/jquery.ui.widget'
 import 'blueimp-file-upload/js/jquery.iframe-transport'
@@ -13,3 +14,7 @@ require('medium-editor-insert-plugin/src/js/images')
 import 'medium-editor-insert-plugin/dist/css/medium-editor-insert-plugin.min.css'
 import 'medium-editor/dist/css/medium-editor.css'
 import 'medium-editor/dist/css/themes/default.css'
+
+MediumEditor.extensions.button.prototype.defaults.h2.contentFA = '<i class="fa fa-heading"></i><sup>2</sup>'
+MediumEditor.extensions.button.prototype.defaults.h3.contentFA = '<i class="fa fa-heading"></i><sup>3</sup>'
+MediumEditor.extensions.button.prototype.defaults.h4.contentFA = '<i class="fa fa-heading"></i><sup>4</sup>'
