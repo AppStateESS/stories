04bf6be7a7ed73ecdf0c5980ccad502021d7be12
Matthew McNaney <mcnaney@gmail.com>
2018-04-16 10:52:55 -0400

    Corrections to navbar to work with bootstrap 4

--- javascript/AddOn/Navbar.js
+++ javascript/AddOn/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import './nav.css'
 
 const Navbar = (props) => {
-  const {rightSide, leftSide, header} = props
+  const {rightSide, leftSide, header,} = props
 
   let url = './'
   let title
@@ -21,76 +21,57 @@ const Navbar = (props) => {
 
   return (
     <div className="stories-navbar">
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#stories-navbar-toggle"
-              aria-expanded="false">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href={url}>{title}</a>
-          </div>
-          <div className="collapse navbar-collapse" id="stories-navbar-toggle">
-            <ul className="nav navbar-nav">
-              <li className="dropdown">
-                <a
-                  className="dropdown-toggle pointer"
-                  data-toggle="dropdown"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false">Manage&nbsp;
-                  <i className="fa fa-chevron-down"></i>
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a href="./stories/Entry/create">
-                      <i className="fa fa-book"></i>&nbsp; Create a new story</a>
-                  </li>
-                  <li role="separator" className="divider"></li>
-                  <li>
-                    <a href="./stories/Listing/admin">
-                      <i className="fa fa-list"></i>&nbsp;Stories</a>
-                  </li>
-                  <li>
-                    <a href="./stories/Feature">
-                      <span className="glyphicon glyphicon-th-large" aria-hidden="true"></span>&nbsp;Features</a>
-                  </li>
-                  <li>
-                    <a href="./stories/Author">
-                      <i className="fa fa-user"></i>&nbsp;Authors</a>
-                  </li>
-                  <li>
-                    <a href="./stories/Settings">
-                      <i className="fa fa-gear"></i>&nbsp;Settings</a>
-                  </li>
-                  <li role="separator" className="divider"></li>
-                  <li>
-                    <a href="index.php?module=controlpanel">Control panel</a>
-                  </li>
-                </ul>
-              </li>
-              {leftSide}
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              {rightSide}
-            </ul>
-          </div>
+      <nav className="navbar navbar-expand-lg fixed-top navbar-light">
+        <a className="navbar-brand" href={url}>{title}</a>
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#stories-navbar-toggle"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="stories-navbar-toggle">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle pointer"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false">Manage
+              </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="./stories/Entry/create">
+                  <i className="fas fa-book"></i>&nbsp; Create a new story</a>
+                <hr/>
+                <a className="dropdown-item" href="./stories/Listing/admin">
+                  <i className="fas fa-list"></i>&nbsp;Stories</a>
+                <a className="dropdown-item" href="./stories/Feature">
+                  <span className="fas fa-th-large"></span>&nbsp;Features</a>
+                <a className="dropdown-item" href="./stories/Author">
+                  <i className="fas fa-user"></i>&nbsp;Authors</a>
+                <a className="dropdown-item" href="./stories/Settings">
+                  <i className="fas fa-cog"></i>&nbsp;Settings</a>
+                <hr/>
+                <a className="dropdown-item" href="index.php?module=controlpanel">Control panel</a>
+              </div>
+            </li>
+            {leftSide}
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            {rightSide}
+          </ul>
         </div>
       </nav>
     </div>
   )
 }
 Navbar.propTypes = {
-  leftSide: PropTypes.oneOfType([PropTypes.array,PropTypes.object,]),
-  rightSide: PropTypes.oneOfType([PropTypes.array,PropTypes.object,]),
-  header: PropTypes.oneOfType([PropTypes.object, PropTypes.string,])
+  leftSide: PropTypes.oneOfType([PropTypes.array, PropTypes.object,]),
+  rightSide: PropTypes.oneOfType([PropTypes.array, PropTypes.object,]),
+  header: PropTypes.oneOfType([PropTypes.object, PropTypes.string,]),
 }
 
 export default Navbar
