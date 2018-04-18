ef78c33b46e390150d6de094b06b97077ceedf56
Matthew McNaney <mcnaney@gmail.com>
2018-04-18 15:17:15 -0400

    Reworked bigcheckbox with font awesome svg changes.

--- javascript/AddOn/BigCheckbox.jsx
+++ javascript/AddOn/BigCheckbox.jsx
@@ -3,46 +3,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import empty from './Empty'
 
-const BigCheckbox = ({handle, checked, label}) => {
+const BigCheckbox = ({handle, checked, label,}) => {
 
   const handleIt = () => {
     handle(empty(checked))
   }
 
-  const mute = {
-    color: '#666'
-  }
-  const point = {
-    cursor: 'pointer',
-    display: 'inline-block',
-  }
-  const labelText = {
-    fontSize: '20px',
-    display: 'inline-block',
-    marginTop: '4px',
-  }
+  const checkBox = (
+    <span className={parseInt(checked) == 1
+        ? 'd-inline'
+        : 'd-none'}>
+      <i className="fas fa-2x fa-check-square mr-1 text-success"></i>
+    </span>
+  )
+  const uncheckBox = (
+    <span className={parseInt(checked) == 1
+        ? 'd-none'
+        : 'd-inline'}>
+      <i className="far fa-2x fa-square mr-1"></i>
+    </span>
+  )
 
   return (
-    <div onClick={handleIt} style={point} className="big-checkbox">
-      <div className="fa-stack fa-lg pull-left">
-        <i className="far fa-square fa-stack-2x" style={mute}></i>
-        {empty(checked)
-          ? null
-          : <i className="fa fa-check text-success fa-stack-2x"></i>}
-      </div>&nbsp;
-      <div
-        style={labelText}
+    <div onClick={handleIt} className="pointer d-flex align-items-center h-100">
+      {checkBox}{uncheckBox}
+      <span
         className={!empty(checked)
-        ? 'text-success'
-        : 'text-muted'}>{label}</div>
+          ? 'text-success'
+          : 'text-muted'}>{label}</span>
     </div>
   )
 }
 
 BigCheckbox.propTypes = {
   label: PropTypes.string,
-  checked: PropTypes.oneOfType([PropTypes.bool, PropTypes.string, PropTypes.number,]),
-  handle: PropTypes.func.isRequired,
+  checked: PropTypes.oneOfType(
+    [PropTypes.bool, PropTypes.string, PropTypes.number,]
+  ),
+  handle: PropTypes.func.isRequired
 }
 
 BigCheckbox.defaultProps = {
