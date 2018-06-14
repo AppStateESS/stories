67f1acb4cacadc25973a81c78e26a6c8b2773e08
Matthew McNaney <mcnaney@gmail.com>
2018-06-14 10:14:50 -0400

    removed jquery include

--- javascript/Tooltip/index.js
+++ javascript/Tooltip/index.js
@@ -1,29 +1,26 @@
-require("expose-loader?$!jquery")
 /* global $ */
 let currentDom = ''
-$(document).ready(() => {
-  $(".tagged").popover({
-    trigger: "manual",
-    html: true,
-    animation: false,
-    placement: 'bottom',
-    content: function () {
-      return currentDom
-    },
-  }).on("mouseenter", function () {
-    var _this = this
-    const entryDom = '#entry-' + $(this).data('entryId')
-    currentDom = $(entryDom).html()
-    $(this).popover("show")
-    $(".popover").on("mouseleave", function () {
-      $(_this).popover('hide')
-    })
-  }).on("mouseleave", function () {
-    var _this = this
-    setTimeout(function () {
-      if (!$(".popover:hover").length) {
-        $(_this).popover("hide")
-      }
-    }, 300)
+$(".tagged").popover({
+  trigger: "manual",
+  html: true,
+  animation: false,
+  placement: 'bottom',
+  content: function () {
+    return currentDom
+  }
+}).on("mouseenter", function () {
+  var _this = this
+  const entryDom = '#entry-' + $(this).data('entryId')
+  currentDom = $(entryDom).html()
+  $(this).popover("show")
+  $(".popover").on("mouseleave", function () {
+    $(_this).popover('hide')
   })
+}).on("mouseleave", function () {
+  var _this = this
+  setTimeout(function () {
+    if (!$(".popover:hover").length) {
+      $(_this).popover("hide")
+    }
+  }, 300)
 })
