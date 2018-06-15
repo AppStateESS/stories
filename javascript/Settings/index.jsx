96ba91ccdcb8dbc3e763e233bc95c65bbaa92d88
Matthew McNaney <mcnaney@gmail.com>
2018-06-15 09:06:19 -0400

    Added global to settings variable

--- javascript/Settings/index.jsx
+++ javascript/Settings/index.jsx
@@ -3,5 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Settings from './Settings.jsx'
 import './style.css'
+/* global settings */
 
 ReactDOM.render(<Settings settings={settings}/>, document.getElementById('Settings'))
