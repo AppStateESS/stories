36bb1c51766739d66f4a7643b97d875c254f49a1
Matthew McNaney <mcnaney@gmail.com>
2018-07-20 12:58:52 -0400

    Added Author resource class

---
+++ javascript/Resource/Author.js
@@ -0,0 +1,10 @@
+export default class Author {
+  constructor() {
+    this.id = 0
+    this.name = ''
+    this.pic = null
+    this.email = ''
+    this.userId = 0
+    this.twitterUsername = ''
+  }
+}
