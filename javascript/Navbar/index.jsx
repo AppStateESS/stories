0c8cf5e30fc0e0ccf2859e8aa3e7418bb10185d4
Matt McNaney <mcnaneym@appstate.edu>
2018-10-03 15:49:08 -0400

    RAdded id to view link to ensure it works.

--- javascript/Navbar/index.jsx
+++ javascript/Navbar/index.jsx
@@ -22,7 +22,7 @@ let leftSide
 if (entry.urlTitle.length > 0) {
   leftSide = (
     <li>
-      <a className="nav-link" href={`./stories/${entry.urlTitle}`}>View story</a>
+      <a className="nav-link" href={`./stories/Entry/${entry.id}/${entry.urlTitle}`}>View story</a>
     </li>
   )
 }
