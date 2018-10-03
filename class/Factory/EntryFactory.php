57c25a9d3721fe370d4a98aecb98b210f8bcd89b
Matt McNaney <mcnaneym@appstate.edu>
2018-10-03 15:51:45 -0400

    Not running clean embed anymore
    Removed cleanFacebook. Not needed with new oEmbed.
    Changed the clean Twitter method but not needed now. Keeping for
    now as it was flakey.

--- class/Factory/EntryFactory.php
+++ class/Factory/EntryFactory.php
@@ -646,6 +646,7 @@ EOF;
     /**
      * Removed the data-embed-code div medium editor adds.
      * @param string $content
+     * @deprecated
      * @return string
      */
     private function cleanEmbed($content)
@@ -656,6 +657,7 @@ EOF;
 
     /**
      * Facebook does zeroes out the container_width
+     * @deprecated
      * @param string $content
      */
     private function cleanFacebook($content)
@@ -669,8 +671,8 @@ EOF;
      * an iframe and moved forward. The iframe is useless, the script and the
      * anchor tag called prior are the important components.
      * 
-     * The true code is stuck in the data-embed-code div. All other embeds have
-     * this cleaned out. This script grabs the embed code and saves it as content
+     * The true code is stuck in the data-embed-code div. This script grabs the 
+     * embed code and saves it as content
      * before it is flushed. This only happens when Flickr is present.
      * @param string $content
      */
@@ -685,13 +687,19 @@ EOF;
         }
         $embedCode = html_entity_decode(html_entity_decode($matches[1]));
         $final = preg_replace('/<div data-embed-code=".*<\/div>/',
-                "<div class=\"medium-insert-active medium-flickr-embed\">$embedCode</div>", $content);
+                "<div class=\"medium-insert-active medium-flickr-embed\">$embedCode</div>",
+                $content);
         return $final;
     }
 
+    /**
+     * @deprecated
+     * @param type $content
+     * @return type
+     */
     private function cleanTwitter($content)
     {
-        return str_replace('<script async="" src="//platform.twitter.com/widgets.js" charset="utf-8"></script>',
+        return preg_replace('/<script.*platform\.twitter\.com[^>]+><\/script>/',
                 '', $content);
     }
 
@@ -711,6 +719,7 @@ EOF;
         return str_replace(' contenteditable="true"', '', $content);
     }
 
+
     /**
      * Cleans up the content string that is imported from Medium Editor.
      * Medium editor content contains remnants of its controls.
@@ -719,9 +728,9 @@ EOF;
     public function filterMedium($content)
     {
         // Removes medium buttons
-        
         $content = trim(preg_replace('/<(div|p) class="medium-insert-buttons".*/s',
                         '', $content));
+
         $content = str_replace('Type caption for image (optional)', '', $content);
         $content = str_replace('Type caption (optional)', '', $content);
         // Removes extra medium paragraphs padded to end of content
@@ -733,8 +742,6 @@ EOF;
         // Removed http:// url from images making them relative
         $content = $this->relativeImages($content);
         $content = $this->cleanFlickr($content);
-        $content = $this->cleanEmbed($content);
-        //$content = $this->cleanFacebook($content);
         //$content = $this->cleanTwitter($content);
         // Removed the contenteditable="true" left in the figcaption
         // (or anywhere else)
