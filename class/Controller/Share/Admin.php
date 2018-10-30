60837644f433f2c4c7e0d4501913c513ebcf5aa3
Matt McNaney <mcnaneym@appstate.edu>
2018-10-30 12:50:39 -0400

    Added methods to remove a share on the host
    + Move share Admin to this controller.

--- class/Controller/Share/Admin.php
+++ class/Controller/Share/Admin.php
@@ -14,6 +14,8 @@ namespace stories\Controller\Share;
 
 use Canopy\Request;
 use stories\Factory\ShareFactory as Factory;
+use stories\Factory\GuestFactory;
+use stories\Factory\HostFactory;
 use stories\View\ShareView as View;
 use stories\Controller\RoleController;
 
@@ -33,23 +35,48 @@ class Admin extends User
     protected function approvePutCommand(Request $request)
     {
         $this->factory->approve($this->id);
-        return ['success'=>true];
+        return ['success' => true];
     }
 
     protected function denyPutCommand(Request $request)
     {
         $this->factory->deny($this->id);
-        return ['success'=>true];
+        return ['success' => true];
     }
 
     public function listUnapprovedJsonCommand(Request $request)
     {
         return ['listing' => $this->factory->listing(0)];
     }
-    
+
     public function deleteCommand(Request $request)
     {
         $this->factory->delete($this->id);
+    }
+
+    public function removePutCommand(Request $request)
+    {
+        $json = $this->factory->removeShare($request);
+        return $json;
+    }
+
+    public function listHtmlCommand(Request $request)
+    {
+        $vars['siteName'] = \Layout::getPageTitle(true);
+        $vars['url'] = \Canopy\Server::getSiteUrl();
+        return $this->view->scriptView('ShareAdmin', true, $vars);
+    }
+
+    public function listJsonCommand(Request $request)
+    {
+        $data = [];
+        $guestFactory = new GuestFactory;
+        $hostFactory = new HostFactory;
+        $data['currentGuests'] = $guestFactory->getCurrentGuests();
+        $data['guestRequests'] = $guestFactory->getGuestRequests();
+        $data['hosts'] = $hostFactory->getHosts();
+        $data['inaccessible'] = $this->factory->getInaccessible();
+        return $data;
     }
 
 }
