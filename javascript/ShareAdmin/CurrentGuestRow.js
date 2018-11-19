1c0b40fe89e12ea3a21f796cf103ad3c74b77fad
Matt McNaney <mcnaneym@appstate.edu>
2018-11-19 12:48:30 -0500

    Removed time from accept date to make room
    + changed delete icon to trash.
    + added authkey popup
    + changed story number listing to a badge

--- javascript/ShareAdmin/CurrentGuestRow.js
+++ javascript/ShareAdmin/CurrentGuestRow.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types'
 import moment from 'moment'
 
 const CurrentGuestRow = ({id, siteName, url, authkey, email, acceptDate, deny, storyCount}) => {
-  const acceptFormatted = moment.unix(acceptDate).format("MMM D YYYY, h:mm a")
+  const acceptFormatted = moment.unix(acceptDate).format("MMM D YYYY")
   return (<tr>
     <td>
       <button className="btn btn-danger btn-sm mr-1" title="Remove guest and prevent submissions" onClick={deny}>
-        <i className="fas fa-times fa-fw"></i>
+        <i className="fas fa-trash-alt fa-fw"></i>
       </button>
     </td>
-    <td><a href={`./stories/Share/guestListing/?guestId=${id}`}>{storyCount}</a></td>
-    <td><a href={url}>{siteName}</a></td>
+    <td><a href={url}>{siteName}</a>&nbsp;<button type="button" className="authkey btn btn-secondary btn-sm" data-toggle="popover" data-content={authkey}><i className="fas fa-key"></i></button></td>
     <td><a href={`mailto:${email}`}>{email}</a></td>
+    <td className="text-center"><a className="badge badge-pill badge-primary" href={`./stories/Share/guestListing/?guestId=${id}`}>{storyCount}</a></td>
     <td>{acceptFormatted}</td>
   </tr>)
 }
