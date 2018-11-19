97dfc3162eee0f2326695ba766945c4ebb1df919
Matt McNaney <mcnaneym@appstate.edu>
2018-11-19 10:27:52 -0500

    Create method to send curl commands.

--- class/Factory/BaseFactory.php
+++ class/Factory/BaseFactory.php
@@ -48,5 +48,16 @@ abstract class BaseFactory extends \phpws2\ResourceFactory
             return ucfirst($name);
         }
     }
+    
+    protected function sendCurl($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+    
 
 }
