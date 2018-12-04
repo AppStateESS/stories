588f13b8f6e75d3dadc23e293fa2e3dcb61a50cd
Matt McNaney <mcnaneym@appstate.edu>
2018-12-04 14:57:43 -0500

    Added note about feature fix.

--- boost/update.php
+++ boost/update.php
@@ -248,6 +248,7 @@ class StoriesUpdate
         $changes[] = 'Character limiter added to urlTitle variable.';
         $changes[] = 'Added id to non-friendly error message.';
         $changes[] = 'Fixed create date relative not working.';
+        $changes[] = 'Fixed Feature stories.';
 
         $this->addContent('1.5.1', $changes);
     }
