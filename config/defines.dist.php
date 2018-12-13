6bc4c038b896f10360ef6c481db3bfca33398952
Matt McNaney <mcnaneym@appstate.edu>
2018-12-13 14:01:18 -0500

    Added twitter-widget back just in case

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -14,7 +14,7 @@ define('STORIES_FRIENDLY_ERROR', true);
 define('STORIES_REACT_DEV', false);
 
 define('STORIES_CONTENT_TAGS',
-        'b,strong,em,i,p,img,iframe,h2,h3,h4,h5,a,style,blockquote,ul,ol,li,figure,figcaption,div,twitterwidget,script,br');
+        'b,strong,em,i,p,img,iframe,h2,h3,h4,h5,a,style,blockquote,ul,ol,li,figure,figcaption,div,twitterwidget,twitter-widget,script,br');
 
 define('STORIES_SUMMARY_TAGS', 'b,strong,em,i,a,style,ul,ol,li,p,br');
 
