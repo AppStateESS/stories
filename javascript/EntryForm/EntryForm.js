a87697f3bfce06e66cd6962c939206980a6b831e
Matt McNaney <mcnaneym@appstate.edu>
2022-04-01 11:29:54 -0400

    Adds code to try to reduce redundant saves.

--- javascript/EntryForm/EntryForm.js
+++ javascript/EntryForm/EntryForm.js
@@ -2,6 +2,19 @@
 
 /* global $ */
 
+String.prototype.hashCode = function () {
+  var hash = 0,
+    i,
+    chr
+  if (this.length === 0) return hash
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
+    hash |= 0 // Convert to 32bit integer
+  }
+  return hash
+}
+
 export default class EntryForm {
   constructor(status, entry) {
     this.entry = entry
@@ -10,6 +23,8 @@ export default class EntryForm {
     }.bind(this)
     // status is the jquery node/object for the status text
     this.status = status
+    this.allowSave = true
+    this.compareHash = null
   }
 
   uploadUrl() {
@@ -26,7 +41,7 @@ export default class EntryForm {
     if (youtubeId && youtubeId.length > 0) {
       $.ajax({
         url: './stories/EntryPhoto/' + this.entry.id,
-        data: {file : youtubeId + '.jpg'},
+        data: {file: youtubeId + '.jpg'},
         dataType: 'json',
         type: 'delete',
         success: function () {
@@ -46,30 +61,49 @@ export default class EntryForm {
 
   save() {
     const entry = this.entry
+    const entryHash = entry.content.hashCode()
+    if (entryHash === this.compareHash) {
+      return
+    } else {
+      this.compareHash = entryHash
+    }
     // Do not try and save content with an base64 encoded image
     if (entry.content.match(/<img src="data:image\//)) {
       return
     }
-    this.status.html('<span><i class="fas fa-cog fa-spin fa-fw"></i>&nbsp;Saving...</span>')
-    $.ajax({
-      url: './stories/Entry/' + entry.id,
-      data: entry,
-      dataType: 'json',
-      type: 'put',
-      success: function () {
-        const ts = new Date()
-        const timestring = ts.toLocaleTimeString('en-US')
-        setTimeout(function(){
-          this.status.html(`<span class="text-success"><i class="fa fa-check"></i>&nbsp;Saved @ ${timestring}</span>`)
-        }.bind(this), 1000)
-      }.bind(this),
-      error: function () {
-        this.status.text('ERROR')
-      }.bind(this)
-    })
-
+    if (!this.allowSave) {
+      return
+    }
+    this.allowSave = false
+    this.limiter = setTimeout(() => {
+      this.status.html(
+        '<span><i class="fas fa-cog fa-spin fa-fw"></i>&nbsp;Saving...</span>'
+      )
+      $.ajax({
+        url: './stories/Entry/' + entry.id,
+        data: entry,
+        dataType: 'json',
+        type: 'put',
+        success: function () {
+          const ts = new Date()
+          const timestring = ts.toLocaleTimeString('en-US')
+          this.allowSave = true
+          clearTimeout(this.limiter)
+          setTimeout(
+            function () {
+              this.status.html(
+                `<span class="text-success"><i class="fa fa-check"></i>&nbsp;Saved @ ${timestring}</span>`
+              )
+            }.bind(this),
+            1000
+          )
+        }.bind(this),
+        error: function () {
+          this.status.text('ERROR')
+        }.bind(this),
+      })
+    }, 2000)
   }
-
 }
 
 window.EntryFormClass = EntryForm
